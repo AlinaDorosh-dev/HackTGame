fdc1ee4b4b8cee90887d12ecdcdeea41238994ab
Ismael <ismaelboulen@gmail.com>
2022-11-08 20:11:51 +0100

    hover funciona

--- TEAM_C/assets/js/app.js
+++ TEAM_C/assets/js/app.js
@@ -1,10 +1,27 @@
 window.addEventListener("load", ()=>{
-    let gamer_img = document.querySelectorAll("#gamers-img-player1");
-    let gamer_plus = document.querySelectorAll("#gamers-plus");
-    gamer_img.addEventListener("focus", (e)=>{
-        document.classList.remove("#gamers-img-player1")
-        document.classList.add("#gamer-img-player1-2")
-        document.classList.remove("gamer-plus")
-        document.classList.add("#gamer-plus-ye")
+    let gamer_img = document.querySelectorAll(".gamers-img-player1");
+    gamer_img.forEach(element => {
+        element.addEventListener("mouseover", () =>{
+            element.classList.remove("gamers-img-player1");
+            element.classList.add("gamers-img-player1-2");
+            element.nextElementSibling.classList.remove("gamers-player1-h3");
+            element.nextElementSibling.classList.add("gamers-player1-h3-hide");
+            element.nextElementSibling.nextElementSibling.classList.remove("gamers-plus");
+            element.nextElementSibling.nextElementSibling.classList.add("gamers-plus-ye");
+            console.log(element.nextElementSibling.nextElementSibling);
+        });
+    });
+    let gamer_plus = document.querySelectorAll(".gamers-plus");
+    gamer_plus.forEach(element =>{
+        element.addEventListener("mouseover", (player)=>{
+            element.classList.remove("gamer-plus-ye");
+            element.classList.add("gamer-plus-wh");
+        });
     })
-})
+});
+
+
+/*element.classList.remove("gamers-plus");
+element.classList.add("gamers-plus-ye");
+element.classList.remove("gamers-player1-h3");
+element.classList.add("gamers-player1-h3-hide");*/
