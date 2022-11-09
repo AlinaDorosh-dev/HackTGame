ae70c5adcab38ef0983d4c7c897f00900a89c2fa
Nelson González <113632938+nel386@users.noreply.github.com>
2022-11-09 12:53:05 +0100

    savingChanges12:50

--- TEAM_C/assets/js/app.js
+++ TEAM_C/assets/js/app.js
@@ -10,4 +10,51 @@ closeMenu.addEventListener("click", () => {
     mainMenu.classList.toggle("main-menu-show");
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JS Alina
+
+let dots = document.querySelectorAll(".fa-circle");
+let image = document.querySelector(".gallery-image");
+let galleryContainer = document.querySelector(".gallery-container");
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", () => {
+    image.src = "img/slider/" + (i + 1) + ".jpg";
+    let active = document.querySelector(".active");
+    active.classList.remove("active");
+    dots[i].classList.add("active");
+      galleryContainer.classList.toggle("flashit");
+      image.classList.toggle("flashit");
+  });
+}
+
+// Get the modal
+let modal = document.getElementById("myModal");
+let login = document.getElementById("login");
+login.addEventListener("click", () => {
+  modal.style.display = "block";
+  console.log("working");
+});
+
+// Get the <span> element that closes the modal
+let closeBtn = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
 });
