bdadc4c301df70434a70a11ffd6819e71672496e
AlinaD <alina.dorosh@gmail.com>
2022-11-10 19:26:57 +0100

    finalizado

--- TEAM_C/assets/js/app.js
+++ TEAM_C/assets/js/app.js
@@ -111,44 +111,28 @@ window.addEventListener("load", () => {
   // JS Ismael
 
   
-    let gamer_img = document.querySelectorAll(".gamers-player1");
-    gamer_img.forEach((element) => {
-      element.addEventListener("mouseover", () => {
-        element.firstElementChild.classList.remove("gamers-img-player1");
-        element.firstElementChild.classList.add("gamers-img-player1-2");
-        element.firstElementChild.nextElementSibling.classList.remove(
-          "gamers-player1-h3"
-        );
-        element.firstElementChild.nextElementSibling.classList.add(
-          "gamers-player1-h3-hide"
-        );
-        element.lastElementChild.classList.remove("gamers-plus");
-        element.lastElementChild.classList.add("gamers-plus-ye");
+  let gamer_img = document.querySelectorAll(".gamers-player1");
+  gamer_img.forEach(element => {
+      element.addEventListener("mouseover", () =>{
+          element.lastElementChild.firstElementChild.classList.remove("gamers-plus");
+          element.lastElementChild.firstElementChild.classList.add("gamers-plus-ye");
       });
-      element.addEventListener("mouseleave", () => {
-        element.firstElementChild.classList.remove("gamers-img-player1-2");
-        element.firstElementChild.classList.add("gamers-img-player1");
-        element.firstElementChild.nextElementSibling.classList.remove(
-          "gamers-player1-h3-hide"
-        );
-        element.firstElementChild.nextElementSibling.classList.add(
-          "gamers-player1-h3"
-        );
-        element.lastElementChild.classList.remove("gamers-plus-ye");
-        element.lastElementChild.classList.add("gamers-plus");
+      element.addEventListener("mouseleave", ()=>{
+          element.lastElementChild.firstElementChild.classList.remove("gamers-plus-ye");
+          element.lastElementChild.firstElementChild.classList.add("gamers-plus");
       });
-    });
-    let gamer_plus = document.querySelectorAll(".gamers-plus");
-    gamer_plus.forEach((element) => {
-      element.addEventListener("mouseover", () => {
-        element.classList.remove("gamer-plus-ye");
-        element.classList.add("gamer-plus-wh");
+  });
+  let gamer_plus = document.querySelectorAll(".gamers-plus");
+  gamer_plus.forEach(element =>{
+      element.addEventListener("mouseover", ()=>{
+          element.classList.remove("gamer-plus-ye");
+          element.classList.add("gamer-plus-wh");
       });
-      element.addEventListener("mouseleave", () => {
-        element.classList.remove("gamer-plus-wh");
-        element.classList.add("gamer-plus-ye");
+      element.addEventListener("mouseleave", ()=>{
+          element.classList.remove("gamer-plus-wh");
+          element.classList.add("gamer-plus-ye");
       });
-    });
+  });
   
   
   
