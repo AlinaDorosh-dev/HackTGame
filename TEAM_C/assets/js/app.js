5c5a911744801d4a94f64eec2d86a02ddf0d37e1
IsmaelBoule <ismaelboulen@gmail.com>
2022-11-10 19:15:06 +0100

    Final!

--- TEAM_C/assets/js/app.js
+++ TEAM_C/assets/js/app.js
@@ -2,20 +2,12 @@ window.addEventListener("load", ()=>{
     let gamer_img = document.querySelectorAll(".gamers-player1");
     gamer_img.forEach(element => {
         element.addEventListener("mouseover", () =>{
-            element.firstElementChild.classList.remove("gamers-img-player1");
-            element.firstElementChild.classList.add("gamers-img-player1-2");
-            element.firstElementChild.nextElementSibling.classList.remove("gamers-player1-h3");
-            element.firstElementChild.nextElementSibling.classList.add("gamers-player1-h3-hide");
-            element.lastElementChild.classList.remove("gamers-plus");
-            element.lastElementChild.classList.add("gamers-plus-ye");
+            element.lastElementChild.firstElementChild.classList.remove("gamers-plus");
+            element.lastElementChild.firstElementChild.classList.add("gamers-plus-ye");
         });
         element.addEventListener("mouseleave", ()=>{
-            element.firstElementChild.classList.remove("gamers-img-player1-2");
-            element.firstElementChild.classList.add("gamers-img-player1");
-            element.firstElementChild.nextElementSibling.classList.remove("gamers-player1-h3-hide");
-            element.firstElementChild.nextElementSibling.classList.add("gamers-player1-h3");
-            element.lastElementChild.classList.remove("gamers-plus-ye");
-            element.lastElementChild.classList.add("gamers-plus");
+            element.lastElementChild.firstElementChild.classList.remove("gamers-plus-ye");
+            element.lastElementChild.firstElementChild.classList.add("gamers-plus");
         });
     });
     let gamer_plus = document.querySelectorAll(".gamers-plus");
@@ -30,9 +22,3 @@ window.addEventListener("load", ()=>{
         });
     });
 });
-
-
-/*element.classList.remove("gamers-plus");
-element.classList.add("gamers-plus-ye");
-element.classList.remove("gamers-player1-h3");
-element.classList.add("gamers-player1-h3-hide");*/
